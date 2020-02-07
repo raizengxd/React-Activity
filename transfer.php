cd4e42ee716b19bca6545550a1f7320c6b74d380
raizengfilosofuxd <rickdavidsangalang@gmail.com>
2020-02-07 09:09:48 +0800

    insert

--- transfer.php
+++ transfer.php
@@ -11,4 +11,13 @@ function message($msg, $link){
 
 message("f","f");
 
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
 ?>
