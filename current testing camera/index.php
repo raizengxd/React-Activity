966d057d5b4651803959f5040a1d5922dc143459
raizengfilosofuxd <rickdavidsangalang@gmail.com>
2020-02-09 00:08:40 +0800

    design  cards

--- current testing camera/index.php
+++ current testing camera/index.php
@@ -31,15 +31,51 @@ nav {
     background-color:#4c46c2;
 }
 body{
-    font-family: Raleway;
+   
 }
 .brand-logo{
     font-weight: bold;
 }
+/* label color */
+
+/* label focus color */
+  /* label color */
+  .input-field label {
+     color: #000;
+   }
+   /* label focus color */
+   .input-field input:focus + label {
+     color: #ff80ab !important;
+   }
+   /* label underline focus color */
+   .row .input-field input[type=text]:focus {
+     border-bottom: 1px solid #ff80ab;
+     box-shadow: 0 1px 0 0 #ff80ab;
+   }
+
+   
+   /* valid color */
+   .input-field input[type=text].valid {
+     border-bottom: 1px solid #9e9e9e;
+     box-shadow: 0 1px 0 0 #9e9e9e;
+     
+   }
+   /* invalid color */
+   .input-field input[type=text].invalid {
+     border-bottom: 1px solid red;
+     box-shadow: 0 1px 0 0 red;
+   }
+   /* icon prefix focus color */
+   .input-field .prefix.active {
+     color: #000;
+   }
+   .fontx{
+    font-weight: bold;
+    font-size:50pt;
+   }
 
 
 
- /* label underline focus color */
 
 
 </style>
@@ -68,18 +104,38 @@ body{
         <li><a href="#">Contact</a></li>
     </ul>
    
-    <div class="container" style="padding-top: 2.5%;">
+    <div class="container" style="padding-top: 4%;">
     <form method="POST" action="insert.php" >
-    <div class="row ">
-    <div class="input-field col s6 ">
-          <input id="last_name" type="text" class="validate" >
-          <label for="last_name">Last Name</label>
+    
+
+   
+
+
+    <div class="row">
+    <div class="col s12 m12">
+      <div class="card">
+        <div class="card-content">
+         
+          <div class="row">
+          <i class="medium material-icons" style="padding-right: 1%;padding-top: %;">event</i><span class="fontx"> Event </span>
+          </div>
+          
+          
+
+          <div class="row ">
+    <div class="input-field col s6">
+          <input id="last_name" type="text" class="validate" placeholder="First Name" >
+          
     </div>
-    <div class="input-field col s6 ">
-          <input id="first_name" type="text" class="validate" >
-          <label for="first_name">First Name</label>
+    
     </div>
+          
+        </div>
+       
+      </div>
     </div>
+  </div>
+    
 
     </form>
     
