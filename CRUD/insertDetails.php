bd7dc77f2dbbbdab7dda58600531fb264fb2ee81
raizengfilosofuxd <rickdavidsangalang@gmail.com>
2020-02-18 20:17:15 +0800

    removed line

--- CRUD/insertDetails.php
+++ CRUD/insertDetails.php
@@ -6,8 +6,7 @@ $email = $_POST['email'];
 $password= $_POST['password'];
 
 $sql = "insert into tbl_login values('$name','$password' )";
-if (mysqli_query($conn, $sql)) {
-        
+if (mysqli_query($conn, $sql)) {        
     echo '<script> alert("New Record") </script>';
     echo "New record created successfully";
     header("Location: index.php");     
