1657ae7a9403ea5e1cedd02890408942e95d7865
raizengxd <rickdavidsangalang@gmail.com>
2020-06-02 22:09:29 +0800

    ass

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './Components/navBar.js';
+
+import React from 'react';
+import NavBar from './Components/navBar.js'
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
-
+        <h1>Hello</h1>
     </div>
   );
 }
