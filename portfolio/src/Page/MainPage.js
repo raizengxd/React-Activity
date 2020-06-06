6934f8dba877245950e71ae2e0f0a497222b931f
raizengxd <rickdavidsangalang@gmail.com>
2020-06-06 17:54:12 +0800

    add

--- portfolio/src/Page/MainPage.js
+++ portfolio/src/Page/MainPage.js
@@ -3,13 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
-
+import './MainPage.css';
 function MainPage() {
   return (
     
         <Container>
-    <Row>
-        <Col sm={8}>sm=8</Col>
+    <Row className="header-2">
+        <Col className="header" sm={8}>
+          
+        </Col>
         <Col sm={4}>sm=4</Col>
     </Row>
     <Row>
