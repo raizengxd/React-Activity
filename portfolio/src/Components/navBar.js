b0265d45e7f6dc00189064527ac7d8c7847dbd34
raizengxd <rickdavidsangalang@gmail.com>
2020-06-19 10:44:56 +0800

    removed zombie codes

--- portfolio/src/Components/navBar.js
+++ portfolio/src/Components/navBar.js
@@ -23,29 +23,7 @@ function NavBar() {
     </Nav>
   </Navbar.Collapse>
 </Navbar>
-        // <Navbar collapseOnSelect expand="lg" variant="dark">
-        //   <Navbar.Brand href="#home">
-        //     <h1 style={{}}>PA</h1>
-        //   </Navbar.Brand>
-        //   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        //   <Navbar.Collapse id="responsive-navbar-nav">
-        //     <Nav className="ml-auto">              
-        //       <Nav.Link href="#features" className="pad-2"> 
-        //       <Icon.House className="pad" />
-        //         Home
-        //         </Nav.Link>
-        //       <Nav.Link href="#pricing" className="pad-2">
-        //       <Icon.Building className="pad"  />
-        //         Projects</Nav.Link>                   
-        //       <Nav.Link href="#deets" className="pad-2">
-        //       <Icon.Person className="pad" />             
-        //         About</Nav.Link >
-        //       <Nav.Link href="#memes" className="pad-2">
-        //       <Icon.ChatDots className="pad" />
-        //         Contact me</Nav.Link>
-        //     </Nav>
-        //   </Navbar.Collapse>
-        // </Navbar>
+       
      
     
   );
