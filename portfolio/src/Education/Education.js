ba1ca3221f950358fcf256ab271db7f3b9feb64c
raizengxd <rickdavidsangalang@gmail.com>
2020-06-19 22:02:54 +0800

    add

---
+++ portfolio/src/Education/Education.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Card} from 'react-bootstrap';
+
+
+function Education() {
+  return (        
+    <div className="education-lay">
+        <div className=""></div>
+    </div>   
+  );
+}
+
+export default Education;
